b17a61dbe51aa566dd87b48b0194798c57f7c651
chemdemo <yangdemo@gmail.com>
2013-11-15 17:30:57 +0800

    dev

---
+++ test/task.js
@@ -0,0 +1,55 @@
+var Task = require('../lib/task');
+
+var mv = function(src, dist) {
+    require('child_process').exec('mv ' + src + ' ' + dist, this.next);
+};
+
+var zip = function() {
+    ;
+};
+
+var task = 
+Task()
+    .register('exportHtml')
+        .step('getWorkData')
+        .step('createDir')
+        .createDir(function() {
+            ;
+        })
+        .step('copy', function() {
+            ;
+        })
+        .step('zip')
+        .pend()
+    .register('exportImg')
+        .step('getWorkData')
+        .step('thumb')
+        .step('mv')
+        .step('zip')
+    .register('exportPdf')
+        .step('getWorkData')
+        .step('thumb')
+        .step('mv')
+        .step('genPdf')
+        .pend()
+    .getWorkData(getWorkData)
+    // .thumb(thumb)
+    // .mv(mv)
+    .zip(zip);
+    // .run();
+
+module.exports = function(req, res, next) {
+    var wid = req.query.wid;
+    var cmd = req.query.cmd;
+    var ext = req.query.ext;
+    var getWorkData = function(wid) {
+        // TODO
+        this.store('workdata', data);
+    };
+    var thumb = function(ext) {
+        var workdata = this.store('workdata');
+    };
+
+    task.mv = mv;
+    task.thumb(thumb, ext);
+};
