a09260e0a8f66b92a67d0b104f7b6815099ea120
chemdemo <yangdemo@gmail.com>
2013-11-22 21:42:14 +0800

    log

--- test/simple.js
+++ test/simple.js
@@ -3,20 +3,20 @@ var Task = require('../lib/TaskMgr');
 var myTask = Task()
     .register('foo')
         .step(function() {
+            var root = this;
             setTimeout(function() {
-                console.log('first');
-                this.done(null, 'first');
+                root.done(null, 'first');
             }, 1000);
         }, 10)
-        .step('bar')
-        .bar(function() {
-            setTimeout(function() {
-                console.log('second');
-                this.done(null, 'second');
-            }, 500);
-        }, 20, 30)
-        .step('biz', function() {
-            this.store('key', 100);
-            this.done(null, 'biz');
-        })
+        // .step('bar')
+        // .bar(function() {
+        //     setTimeout(function() {
+        //         console.log('second');
+        //         this.done(null, 'second');
+        //     }, 500);
+        // }, 20, 30)
+        // .step('biz', function() {
+        //     this.store('key', 100);
+        //     this.done(null, 'biz');
+        // })
     .run();
