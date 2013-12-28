2073a1da3897269b48164986c49c2e42ac36d2c3
chemdemo <yangdemo@gmail.com>
2013-12-29 01:01:56 +0800

    fix bugs with domain module in node v0.8

---
+++ test/domain_demo.js
@@ -0,0 +1,33 @@
+var should = require('should');
+
+var domain = require('domain');
+var http = require('http');
+var fs = require('fs');
+
+// see: http://www.slideshare.net/domenicdenicola/domains-20010482
+
+describe('active domain not created case', function() {
+    it('should has no active domain if `domain.create()` not called', function() {
+        should.not.exist(process.domain);
+    });
+
+    it('should has active domain after `domain.create()` called', function() {
+        var d = domain.create();
+
+        d.on('error', function(err) {
+            console.log(err.domain);
+            // d.exit();
+        });
+
+        // makes d the current domain
+        d.enter();
+
+        process.domain.should.be.an.Object;
+        process.domain.should.equal(domain.active);
+
+        // makes d inactive
+        d.exit();
+
+        should.not.exist(process.domain);
+    });
+});
--- test/task.js
+++ test/task.js
@@ -383,31 +383,33 @@ describe('Stepify', function() {
                     d.exit();
                 });
 
-                d.run(function() {
-                    Stepify()
-                        .step(function() {
-                            var root = this;
-                            setTimeout(function() {
-                                c1++;
-                                root.done(null);
-                            }, 200);
-                        })
-                        .step(function() {
-                            var root = this;
-                            setTimeout(function() {
-                                c1++;
-                                root.done('There sth error!');
-                            }, 100);
-                        })
-                        .step(function() {
-                            var root = this;
-                            setTimeout(function() {
-                                c1++;
-                                root.done(null);
-                            }, 300);
-                        })
-                        .run();
-                });
+                d.enter();
+
+                Stepify()
+                    .step(function() {
+                        var root = this;
+                        setTimeout(function() {
+                            c1++;
+                            root.done(null);
+                        }, 200);
+                    })
+                    .step(function() {
+                        var root = this;
+                        setTimeout(function() {
+                            c1++;
+                            root.done('There sth error!');
+                        }, 100);
+                    })
+                    .step(function() {
+                        var root = this;
+                        setTimeout(function() {
+                            c1++;
+                            root.done(null);
+                        }, 300);
+                    })
+                    .run();
+
+                d.exit();
             });
         });
 
@@ -446,54 +448,49 @@ describe('Stepify', function() {
 
         describe('use customed errorHandle and multiply tasks case', function() {
             it('should stop executing immediate error occured', function(done) {
-                var d = domain.create();
-
-                d.on('error', function(err) {
-                    err.message.should.equal('The file not_exist.js was not found.');
-                    done();
-                    d.exit();
-                });
-
-                d.run(function() {
-                    Stepify()
-                        .task('foo')
-                            .step(function() {
-                                var root = this;
-                                setTimeout(function() {
-                                    c3++;
-                                    root.done(null);
-                                }, 300);
-                            })
-                            .step(function() {
-                                var root = this;
-                                fs.readFile(path.join(__dirname, 'not_exist.js'), function(err) {
-                                    c3++;
-                                    if(err) err = 'The file not_exist.js was not found.';
-                                    root.done(err);
-                                });
-                            })
-                            .error(function(err) {
-                                throw new Error('The file not_exist.js was not found.');
-                            })
-                        .pend()
-                        .task('bar')
-                            .step(function() {
-                                var root = this;
-                                setTimeout(function() {
-                                    c3++;
-                                    root.done(null);
-                                }, 300);
-                            })
-                            .step(function() {
-                                var root = this;
-                                setTimeout(function() {
-                                    c3++;
-                                    root.done('should not executed ever.');
-                                }, 100);
-                            })
-                        .run();
-                });
+                Stepify()
+                    .task('foo')
+                        .step(function() {
+                            var root = this;
+                            setTimeout(function() {
+                                c3++;
+                                root.done(null);
+                            }, 300);
+                        })
+                        .step(function() {
+                            var root = this;
+                            fs.readFile(path.join(__dirname, 'not_exist.js'), function(err) {
+                                c3++;
+                                if(err) err = 'The file not_exist.js was not found.';
+                                root.done(err);
+                            });
+                        })
+                        .error(function(err) {
+                            err.should.equal('The file not_exist.js was not found.');
+                            done();
+                        })
+                    .pend()
+                    .task('bar')
+                        .step(function() {
+                            var root = this;
+                            setTimeout(function() {
+                                c3++;
+                                root.done(null);
+                            }, 300);
+                        })
+                        .step(function() {
+                            var root = this;
+                            setTimeout(function() {
+                                c3++;
+                                root.done('should not executed ever.');
+                            }, 100);
+                        })
+                    .run();
             });
+        });
+
+        after(function() {
+            c3.should.equal(2);
         });
     });
 
