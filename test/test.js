226c85385dc4a5b793d3064f7248b498ef4beff4
dmyang <yangdemo@gmail.com>
2013-12-31 17:54:20 +0800

    Delete test.js

--- test/test.js
+++
@@ -1,7 +0,0 @@
-var Stepify = require('../index');
-var should = require('should');
-var fs = require('fs');
-var path = require('path');
-var domain = require('domain');
-var exec = require('child_process').exec;
-
