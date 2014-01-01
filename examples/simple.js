e930c697a4fdb895fb7d529c2f3c651e14df229e
chemdemo <yangdemo@gmail.com>
2014-01-02 01:41:28 +0800

    add simple exampls

--- examples/simple.js
+++ examples/simple.js
@@ -1,22 +1,56 @@
 var request = require('request');
 var Stepify = require('../index');
 
-// simple github authorize
+// Authorizing based on oauth2 workflow
 Stepify()
-    .step('getCode', function(cId, rUri) {
-        // get code
+    .step('getCode', function(appId, rUri) {
         var root = this;
-        request.get(
-            'https://github.com/login/oauth/authorize?client_id=' + cId + '&redirect_uri=' + rUri,
-            function(err, res, body) {
-                root.done(err, );
-            }
-        );
-    }, 'da99f7d527e218b87442', 'http://oauth.dmfeel.com/auth/github/callback')
-    .step('getToken', function() {
-        ;
+        request.get('[authorize_uri]', function(err, res, body) {
+            root.done(err, JSON.parse(body).code);
+        });
+    }, [appId], [redirectUri])
+    .step('getToken', function(code) {
+        var root = this;
+        request.post('[token_uri]', function(err, res, body) {
+            root.done(err, JSON.parse(body).access_token);
+        });
     })
-    .step('getInfo', function() {
-        ;
+    .step('getInfo', function(token) {
+        request.get('[info_uri]?token=' + token, function(err, res, body) {
+            // got user info, return to client via http response
+        });
+    })
+    .run();
+
+// muitiply tasks case
+Stepify()
+    .step(function() {
+        var root = this;
+        setTimeout(function() {
+            // do some stuff ...
+            root.done(null, 'a');
+        }, 100);
+    })
+    .step(function() {
+        var root = this;
+        setTimeout(function() {
+            // do some stuff ...
+            root.done();
+        }, 100);
+    })
+    .pend()
+    .step(function() {
+        var root = this;
+        setTimeout(function() {
+            // do some stuff ...
+            root.done();
+        }, 200);
+    })
+    .step(function() {
+        var root = this;
+        setTimeout(function() {
+            // do some stuff ...
+            root.done();
+        }, 200);
     })
     .run();
